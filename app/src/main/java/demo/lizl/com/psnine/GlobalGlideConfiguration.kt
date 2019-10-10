4adcb05eb01130367cdc41e50b8add718200efe7
李志伦 <313701855@qq.com>
2019-10-10 15:44:20 +0800

    采用permissionsdispatcher进行权限获取

---
+++ app/src/main/java/demo/lizl/com/psnine/GlobalGlideConfiguration.kt
@@ -0,0 +1,54 @@
+package demo.lizl.com.psnine
+
+import android.content.Context
+import android.os.Environment
+import com.bumptech.glide.GlideBuilder
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.load.engine.bitmap_recycle.LruBitmapPool
+import com.bumptech.glide.load.engine.cache.DiskLruCacheFactory
+import com.bumptech.glide.load.engine.cache.LruResourceCache
+import com.bumptech.glide.module.AppGlideModule
+import java.io.File
+
+@GlideModule class GlobalGlideConfiguration : AppGlideModule()
+{
+    private val DISK_CACHE_SIZE = 1024 * 1024 * 2000L //最多可以缓存多少字节的数据
+    private val DISK_CACHE_NAME = "psnine_glide"
+
+    override fun applyOptions(context: Context, builder: GlideBuilder)
+    {
+
+        //1.设置Glide内存缓存大小
+        val maxMemory = Runtime.getRuntime().maxMemory().toInt() //获取系统分配给应用的总内存大小
+        val memoryCacheSize = maxMemory / 8 //设置图片内存缓存占用八分之一
+        //设置内存缓存大小
+        builder.setMemoryCache(LruResourceCache(memoryCacheSize.toLong()))
+
+        // 2.设置Glide磁盘缓存大小
+        val cacheDir = createSavePath() //指定的是数据的缓存地址
+
+        //3.设置磁盘缓存大小
+        builder.setDiskCache(DiskLruCacheFactory(cacheDir.path, DISK_CACHE_NAME, DISK_CACHE_SIZE))
+
+        //3.设置BitmapPool缓存内存大小
+        builder.setBitmapPool(LruBitmapPool(memoryCacheSize.toLong()))
+    }
+
+    override fun isManifestParsingEnabled() = false
+
+    /**
+     * 创建存储缓存的文件夹
+     */
+    private fun createSavePath(): File
+    {
+        val path = Environment.getExternalStorageDirectory().path + "/psnine/imageCache/"
+
+        val file = File(path)
+
+        if (!file.exists())
+        {
+            file.mkdirs()
+        }
+        return file
+    }
+}
--- app/src/main/java/demo/lizl/com/psnine/mvp/activity/MainActivity.kt
+++ app/src/main/java/demo/lizl/com/psnine/mvp/activity/MainActivity.kt
@@ -1,31 +1,30 @@
 package demo.lizl.com.psnine.mvp.activity
 
 import android.Manifest
-import android.content.pm.PackageManager
-import com.google.android.material.tabs.TabLayout
-import androidx.core.app.ActivityCompat
+import android.util.Log
 import androidx.fragment.app.Fragment
-import androidx.core.content.ContextCompat
+import com.google.android.material.tabs.TabLayout
 import demo.lizl.com.psnine.R
 import demo.lizl.com.psnine.adapter.FragmentPageAdapter
 import demo.lizl.com.psnine.customview.dialog.BaseDialog
 import demo.lizl.com.psnine.customview.dialog.DialogOperationConfirm
+import demo.lizl.com.psnine.mvp.contract.EmptyContract
 import demo.lizl.com.psnine.mvp.fragment.GameFragment
 import demo.lizl.com.psnine.mvp.fragment.HomeFragment
 import demo.lizl.com.psnine.mvp.fragment.UserFragment
-import demo.lizl.com.psnine.mvp.contract.EmptyContract
 import demo.lizl.com.psnine.mvp.presenter.EmptyPresenter
-import demo.lizl.com.psnine.util.ToastUtil
 import demo.lizl.com.psnine.util.UiUtil
 import kotlinx.android.synthetic.main.activity_main.*
+import permissions.dispatcher.NeedsPermission
+import permissions.dispatcher.OnNeverAskAgain
+import permissions.dispatcher.OnPermissionDenied
+import permissions.dispatcher.RuntimePermissions
 
-class MainActivity : BaseActivity<EmptyPresenter>(), EmptyContract.View
+@RuntimePermissions class MainActivity : BaseActivity<EmptyPresenter>(), EmptyContract.View
 {
 
     private lateinit var fragmentList: List<Fragment>
     private var dialogOperationConfirm: DialogOperationConfirm? = null
-
-    private val REQUEST_CODE_READ_EX_PERMISSION = 23
 
     override fun getLayoutResId(): Int
     {
@@ -36,7 +35,7 @@ class MainActivity : BaseActivity<EmptyPresenter>(), EmptyContract.View
 
     override fun initView()
     {
-        checkReadStoragePermission()
+        checkReadStoragePermissionWithPermissionCheck()
         initViewPage()
     }
 
@@ -54,44 +53,50 @@ class MainActivity : BaseActivity<EmptyPresenter>(), EmptyContract.View
         tl_bottom.addOnTabSelectedListener(TabLayout.ViewPagerOnTabSelectedListener(vp_fragment_container))
     }
 
-    /**
-     * 检查内部存储读取权限
-     */
-    private fun checkReadStoragePermission(): Boolean
+    @NeedsPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) fun checkReadStoragePermission()
     {
-        // 权限以获取直接返回
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)
-        {
-            return true
-        }
-        // 权限被拒绝过但是用户没有设置权限弹窗不再弹出的情况下继续申请权限
-        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE))
-        {
-            ActivityCompat.requestPermissions(
-                    this, arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), REQUEST_CODE_READ_EX_PERMISSION
-            )
-        }
-        // 权限被拒绝且不再允许弹出申请权限弹窗的情况下弹出跳转到APP详情确认弹窗（用于重新设置权限）
-        else
-        {
-            dialogOperationConfirm = DialogOperationConfirm(this, getString(R.string.notify_failed_to_get_permission), getString(R.string.notify_permission_be_refused))
-            dialogOperationConfirm?.setOnConfirmButtonClickListener(object : BaseDialog.OnConfirmButtonClickListener
-            {
-                override fun onConfirmButtonClick()
-                {
-                    UiUtil.goToAppDetailPage()
-                }
-            })
-            dialogOperationConfirm?.show()
-        }
-        return false
+        Log.d(TAG, "checkReadStoragePermission")
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray)
+    @OnPermissionDenied(Manifest.permission.WRITE_EXTERNAL_STORAGE) fun onPermissionDenied()
     {
-        if (grantResults.isNotEmpty() && grantResults[0] != PackageManager.PERMISSION_GRANTED)
+        Log.d(TAG, "onPermissionDenied")
+
+        dialogOperationConfirm =
+            DialogOperationConfirm(this, getString(R.string.notify_failed_to_get_permission), getString(R.string.notify_permission_be_refused))
+        dialogOperationConfirm?.setOnConfirmButtonClickListener(object : BaseDialog.OnConfirmButtonClickListener
         {
-            ToastUtil.showToast(R.string.notify_failed_to_get_permission)
-        }
+            override fun onConfirmButtonClick()
+            {
+                checkReadStoragePermissionWithPermissionCheck()
+            }
+        })
+        dialogOperationConfirm?.show()
+    }
+
+    @OnNeverAskAgain(Manifest.permission.WRITE_EXTERNAL_STORAGE) fun onPermissionNeverAskAgain()
+    {
+        Log.d(TAG, "onPermissionNeverAskAgain")
+
+        dialogOperationConfirm = DialogOperationConfirm(
+            this, getString(R.string.notify_failed_to_get_permission), getString(R.string.notify_permission_be_refused_and_never_ask_again)
+        )
+        dialogOperationConfirm?.setOnConfirmButtonClickListener(object : BaseDialog.OnConfirmButtonClickListener
+        {
+            override fun onConfirmButtonClick()
+            {
+                UiUtil.goToAppDetailPage()
+            }
+        })
+        dialogOperationConfirm?.show()
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray)
+    {
+        Log.d(TAG, "onRequestPermissionsResult")
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        // NOTE: delegate the permission handling to generated function
+        onRequestPermissionsResult(requestCode, grantResults)
     }
 }
